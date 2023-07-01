afc588877f51be0151993be96b5e4d3711541ad4
timmac62 <25373189+timmac62@users.noreply.github.com>
2023-07-01 07:50:11 -0400

    Initial Implementation

---
+++ busyBee.py
@@ -0,0 +1,33 @@
+import time
+import board
+import digitalio
+import neopixel
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.2, auto_write=False)
+
+mode_button = digitalio.DigitalInOut(board.SLIDE_SWITCH)
+mode_button.direction = digitalio.Direction.INPUT
+mode_button.pull = digitalio.Pull.UP
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+WHITE = (255, 255, 255)
+OFF = (0, 0, 0)
+
+flash_rate = 0.25
+
+while True:
+    if mode_button.value is True:
+        pixels.fill(RED)
+        pixels.show()
+        time.sleep(flash_rate)
+        pixels.fill(OFF)
+        pixels.show()
+        time.sleep(flash_rate)
+    else:
+        pixels.fill(OFF)
+        pixels.show()
---
+++ updateACE.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+# cp busyBee.py /Volumes/CIRCUITPY/code.py
+# reset ACE
+print(f"update code.py on ACE")
